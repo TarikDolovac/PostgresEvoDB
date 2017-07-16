3c044061fb97f77e1a2caba4d8818e6ee0e82b0f
Tarik Dolovac <tarik.dolovac@ministryofprogramming.com>
2017-07-16 13:54:07 +0200

    Adding new column to table, alter function to support new column

--- db_scripts/dbobjects/cfg/functions/get_users.sql
+++ db_scripts/dbobjects/cfg/functions/get_users.sql
@@ -1,8 +1,8 @@
 #Creating function which return all users
 
 Create or replace function cfg.get_users () 
-returns table (ID int, name text)
+returns table (ID int, name text, active bit)
 as $$
-	Select userID, username from cfg.users 
+	Select userID, username, is_active from cfg.users 
 	$$
 language SQL;
--- db_scripts/dbobjects/cfg/tables/users.sql
+++ db_scripts/dbobjects/cfg/tables/users.sql
@@ -7,4 +7,8 @@ create table cfg.users
 		,username varchar (55)
 		,password varchar (15)
 		,createdate date
-		)
+		);
+
+alter table cfg.users add column is_active bit;
+
+alter table cfg.users alter column userid set default nextval('cfg.seq_users');
