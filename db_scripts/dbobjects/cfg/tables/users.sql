0be1a4266106a54fa8544f2aa751ce316145ac3b
Tarik Dolovac <tarik.dolovac@ministryofprogramming.com>
2017-07-16 15:54:43 +0200

    comment sql code

--- db_scripts/dbobjects/cfg/tables/users.sql
+++ db_scripts/dbobjects/cfg/tables/users.sql
@@ -6,8 +6,10 @@ create table if not exist cfg.users
 		,createdate date
 		);
 
+/*
 alter table cfg.users add column is_active bit;
 
 alter table cfg.users alter column userid set default nextval('cfg.seq_users');
 
 alter table cfg.users alter column is_active set default (1::bit);
+*/
