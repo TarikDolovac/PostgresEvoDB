7175c3f7a52a15383262d5fd93707a9380733efb
Tarik Dolovac <tarik.dolovac@ministryofprogramming.com>
2017-07-16 15:56:40 +0200

    remove not exists code

--- db_scripts/dbobjects/cfg/tables/users.sql
+++ db_scripts/dbobjects/cfg/tables/users.sql
@@ -1,4 +1,4 @@
-create table if not exist cfg.users
+create table cfg.users
 	(
 		userid int
 		,username varchar (55)
@@ -6,10 +6,9 @@ create table if not exist cfg.users
 		,createdate date
 		);
 
-/*
+
 alter table cfg.users add column is_active bit;
 
 alter table cfg.users alter column userid set default nextval('cfg.seq_users');
 
 alter table cfg.users alter column is_active set default (1::bit);
-*/
