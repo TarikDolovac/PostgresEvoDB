22f2192700ec7f0533f8c54db089c64e70ba6446
Tarik Dolovac <tarik.dolovac@ministryofprogramming.com>
2017-07-16 14:01:37 +0200

    set default value for is_active column in users table

--- db_scripts/dbobjects/cfg/tables/users.sql
+++ db_scripts/dbobjects/cfg/tables/users.sql
@@ -1,7 +1,7 @@
 #creating table in cfg schema
 #table will contain all users for loging on system
 
-create table cfg.users
+create table if not exist cfg.users
 	(
 		userid int
 		,username varchar (55)
@@ -12,3 +12,5 @@ create table cfg.users
 alter table cfg.users add column is_active bit;
 
 alter table cfg.users alter column userid set default nextval('cfg.seq_users');
+
+alter table cfg.users alter column is_active set default (1::bit);
